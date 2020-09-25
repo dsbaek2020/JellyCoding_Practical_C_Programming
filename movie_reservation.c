f4b863164c95c14b2fecffe2ac2df1565cde385f
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2020-09-25 16:36:04 +0900

    Update movie_reservation.c
     프로그램의 흐름을 상태머신형태로 코딩

--- movie_reservation.c
+++ movie_reservation.c
@@ -48,34 +48,49 @@ int diplayMainPanel(MOVIE_INFO *ptr)
   int select;
 
   printf("영화 예약 프로그램\n");
-  diplayTitle(ptr, 0);
-  diplayTitle(ptr+1, 1);
-  diplayTitle(ptr+2, 2);
+  diplayTitle(ptr, 1);
+  diplayTitle(ptr+1, 2);
+  diplayTitle(ptr+2, 3);
 
-  printf("영화를 선택해 주세요");
+  printf("영화를 선택해 주세요.  0번을 누르시면 종료합니다.");
   scanf("%d", &select);  //blocking 함수 
-  return select;
+  return select-1;
 }
 
-void movieReservation(MOVIE_INFO *ptr){
-   int selectSeat
-   printf("%s \n",  ptr->movieTitle);
-   printf("%s \n",  ptr->ActorName);
+int movieReservation(MOVIE_INFO *ptr){
+   int selectSeat;
+   int returnValue;
+
+   printf("제목:");  printf("%s \n",  ptr->movieTitle);
+   printf("배우:");  printf("%s \n",  ptr->ActorName);
 
   //-----------좌석상태 표시----
 
 
   //----------------------------
+  int reservation =0;
+  do{
 
-  printf("영화를 선택해 주세요(1~16번). 0번을 누르시면 취소 입니다. ");
-  scanf("%d", &selectSeat);  //blocking 함수 
+    printf("좌석을 선택해 주세요(1~16번). 0번을 누르시면 취소 입니다. \n");
+    scanf("%d", &selectSeat);  //blocking 함수 
+    if(selectSeat == 0){
+      printf("취소 되었습니다. 메인 매뉴로 돌아갑니다.\n");
+      returnValue = -1;
+      break;
+    }
 
-  //----------------------------
-  if( ptr->seat.seat.seat[selectSeat] == 'O')
-  {
-    ptr->seat.seat.seat[selectSeat] = 'V';
-    printf("%d번으로 예약되었습니다. ", selectSeat);
-  }
+    //----------------------------
+    if( ptr->seat.seat.seat[selectSeat-1] == 'O')
+    {
+      ptr->seat.seat.seat[selectSeat-1] = 'V';
+      printf("%d번으로 예약되었습니다. \n", selectSeat);
+      reservation =1;
+      returnValue = selectSeat;
+    }
+    else{
+      printf("%d번 좌석은 이미 예약된 자리입니다. 다른자리를 선택해주세요.\n", selectSeat);
+    }
+  }while(reservation != 1);
 
   
 
@@ -84,11 +99,19 @@ void movieReservation(MOVIE_INFO *ptr){
 
   //----------------------------
 
+  return returnValue;
+
 
 }
 
+#define LOGIN           0
+#define MAIN            1
+#define RESERVATION     2
+#define EXIT            3
+
+char ProgramStatus = MAIN;
+
 int main(void) {
-  char userCommand[10];
   int selectedMovie;
 
   // 영화정보가 모두 포함된 구조체 변수 선언
@@ -103,16 +126,26 @@ int main(void) {
 
   do
   {
+     if(ProgramStatus == MAIN){
+        selectedMovie = diplayMainPanel(&movies[0]);
 
-     selectedMovie = diplayMainPanel(&movies[0]);
+        if(selectedMovie > 0)
+        {
+          ProgramStatus = RESERVATION;
+        }
+        else{
+          ProgramStatus = EXIT;
+        }
+     }else if(ProgramStatus == RESERVATION){
 
-     movieReservation(&movies[selectedMovie]);
+        if(movieReservation(&movies[selectedMovie]) == -1)
+        {
+          ProgramStatus = MAIN;
+        }
 
-     
-     printf("나가시겠습니까?(y/n");
-     scanf("%s", &userCommand);  //blocking 함수 
+     }
 
-  }while(   strcmp(userCommand, "y") != 0);
+  }while( ProgramStatus != EXIT);
 
   printf("감사합니다");
 
